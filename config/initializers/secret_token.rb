4069f240d4123a0f2e3b33fbd4d83dc298811bdb
hiroki_iwasaki <hiroki_iwasaki@kogasoftware.com>
2013-11-26 10:51:29 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PokemonParameter::Application.config.secret_token = '74c6a486adea5b15840a71f5b6254e1bedadee68746bb4fab1e4031143f132fd827bb950db794af97cb49fcf2b43e54dea10f2d3c380eae44e6b8c1491d94ecf'
